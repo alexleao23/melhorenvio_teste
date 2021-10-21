32ef19784370d94618ad74273c903f39b7b3727b
Alexander Leão <alxndrleao@gmail.com>
2021-10-21 10:42:44 -0300

    Pequenos ajustes para tentar teste da api

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,28 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
+import {
+  VercelRequest,
+  VercelResponse
+} from '@vercel/node';
 import dotenv from 'dotenv';
 import axios from 'axios';
-import { ICalculate, IWebHook, ICalculateProduct, IItems } from '../interfaces/webhook';
+import {
+  ICalculate,
+  IWebHook,
+  ICalculateProduct,
+  IItems
+} from '../interfaces/webhook';
 
 dotenv.config();
 
-export default async(req: VercelRequest, res: VercelResponse): Promise<void> => {
-    const rates: any = [];
-    const { content }: IWebHook = req.body;
-    const { billingAddressPostalCode, shippingAddressPostalCode, items } = content;
-    try {
-        const { data } = await sendPostalCodeAndItems(billingAddressPostalCode, shippingAddressPostalCode, items);
-        data.forEach((item: any) => {
-          if (!item.error) {
-            rates.push({ 
-              cost: item.price, 
-              description: `${item.company.name} - ${item.name.replace('.', '')}`, 
-              guaranteedDaysToDelivery: item.delivery_time,
-            });
-          }
-        });
-        // response for snipcart
-        res.send({ rates });
-    }
-    catch (err) {
-        console.log(err);
-        res.send({
-          errors: [{
-            key: 'error_api_melhor_envio',
-            message: 'Erro ao processar na melhor envio'
-          }]
-        });
-    }
-};
-
 // send data for melhor envio
-const sendPostalCodeAndItems = (billingPostalCode: string, shippingPostalCode: string, items: IItems[]) => {
-  console.log({
-    billingPostalCode,
-    shippingPostalCode
-  })
+const sendPostalCodeAndItems = async (
+  billingPostalCode: string,
+  shippingPostalCode: string,
+  items: IItems[]
+) => {
+  // console.log({
+  //   billingPostalCode,
+  //   shippingPostalCode
+  // });
   const token: string | undefined = process.env.TOKEN_MELHOR_ENVIO;
   const products: ICalculateProduct[] = [];
   items.forEach((item) => {
@@ -63,7 +46,7 @@ const sendPostalCodeAndItems = (billingPostalCode: string, shippingPostalCode: s
     products,
   };
 
-  console.log(dataBody);
+  // console.log(dataBody);
 
   const headers = {
     'Authorization': `Bearer ${token}`,
@@ -72,7 +55,39 @@ const sendPostalCodeAndItems = (billingPostalCode: string, shippingPostalCode: s
     'User-Agent': process.env.USER_AGENT
   };
   const url_api = process.env.URL_MELHOR_ENVIO;
-  return axios.post(`${url_api}/api/v2/me/shipment/calculate`, dataBody, {
+  return await axios.post(`${url_api}/api/v2/me/shipment/calculate`, dataBody, {
     headers,
   });
 }
+
+export default async(
+  req: VercelRequest,
+  res: VercelResponse
+): Promise<void> => {
+    const rates: any = [];
+    const { content }: IWebHook = req.body;
+    const { billingAddressPostalCode, shippingAddressPostalCode, items } = content;
+    try {
+        const { data } = await sendPostalCodeAndItems(billingAddressPostalCode, shippingAddressPostalCode, items);
+        data.forEach((item: any) => {
+          if (!item.error) {
+            rates.push({ 
+              cost: item.price, 
+              description: `${item.company.name} - ${item.name.replace('.', '')}`, 
+              guaranteedDaysToDelivery: item.delivery_time,
+            });
+          }
+        });
+        // response for snipcart
+        res.send({ rates });
+    }
+    catch (err) {
+        // console.log(err);
+        res.send({
+          errors: [{
+            key: 'error_api_melhor_envio',
+            message: 'Erro ao processar na melhor envio'
+          }]
+        });
+    }
+};
